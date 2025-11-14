894f305737efaf5d60cefdfd0bf87f95806ca267
aisha gilani <aishaagilani@gmail.com>
2025-11-14 18:22:06 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,9 @@
+class Test {
+    void cleanup() {
+        System.out.println("Object is garbage collected");
+    }
+    public static void main(String[] args) {
+        Test obj = new Test();
+        obj.cleanup();  
+    }
+}
